c416b0100787e1b7d96bc564b203f3a2883f29e5
Cristian Ciuprin <47356630+ciuprin1@users.noreply.github.com>
2023-10-02 10:31:59 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+export function sumFunc(num1, num2) {
+  console.log(num1 + num2);
+}
+
+export function strFunc(str1, str2) {
+  console.log(str1 + str2);
+}
+export function obj1Func() {
+  console.log(arguments);
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import {sumFunc, strFunc, obj1Func} from "./functions.js"
+
+sumFunc(5, 6);
+
+strFunc("Cristian ", "Ciuprin");
+
+obj1Func(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
